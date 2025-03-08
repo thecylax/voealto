54bfe993c01e530d306c11b40f0f972ff502c767
Robson <cardoso.rcs@gmail.com>
2025-03-08 07:43:34 -0300

    Update settings.py

--- voealto/settings.py
+++ voealto/settings.py
@@ -154,7 +154,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'app/static',
 ]
-STATIC_ROOT = 'app/staticfiles'
+STATIC_ROOT = '/app/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
